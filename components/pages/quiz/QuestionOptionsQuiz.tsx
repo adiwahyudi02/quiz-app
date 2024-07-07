738939b3a2b15421dbf4e1ef08375a2fab41b63d
Adi Wahyudi <adi.whyd02@gmail.com>
2024-07-07 14:36:26 +0700

    feat: crate QuestionOptionsQuiz component

---
+++ components/pages/quiz/QuestionOptionsQuiz.tsx
@@ -0,0 +1,65 @@
+import { Answer } from "@/types/answer";
+import { shuffleArray } from "@/utils/shuffleArray";
+import { Button, Stack } from "@mui/material";
+
+interface IQuestionOptionsQuiz {
+  questionId: number;
+  options: string[];
+  isAnswered: boolean;
+  answered: string;
+  isCorrect: boolean;
+  onCheckAnswer: (answer: Answer) => void;
+}
+
+export const QuestionOptionsQuiz = ({
+  questionId,
+  options = [],
+  isAnswered,
+  answered,
+  isCorrect,
+  onCheckAnswer,
+}: IQuestionOptionsQuiz) => {
+  const shuffledOptions = shuffleArray(options);
+
+  return (
+    <Stack spacing={1}>
+      {!isAnswered ? (
+        shuffledOptions.map((option) => (
+          <Button
+            key={option}
+            variant="outlined"
+            sx={{
+              fontWeight: 700,
+              justifyContent: "start",
+              textTransform: "none",
+              padding: "15px",
+              borderRadius: "10px",
+            }}
+            onClick={() =>
+              onCheckAnswer({
+                question_id: questionId,
+                answer: option,
+              })
+            }
+          >
+            {option}
+          </Button>
+        ))
+      ) : (
+        <Button
+          variant="contained"
+          color={isCorrect ? "success" : "error"}
+          sx={{
+            fontWeight: 700,
+            justifyContent: "start",
+            textTransform: "none",
+            padding: "15px",
+            borderRadius: "10px",
+          }}
+        >
+          {answered}
+        </Button>
+      )}
+    </Stack>
+  );
+};
