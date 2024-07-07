f94720eb0e4f4f375f0e14b0d9f597f1e4e4b6dd
Adi Wahyudi <adi.whyd02@gmail.com>
2024-07-08 02:48:38 +0700

    test: create mockAnswersData

---
+++ __mocks__/mocksAnswersData.ts
@@ -0,0 +1,14 @@
+export const mocksAnswersData = [
+  {
+    question_id: 1,
+    answer: "7 o'clock",
+  },
+  {
+    question_id: 2,
+    answer: "Take a shower and get dressed",
+  },
+  {
+    question_id: 3,
+    answer: "English and math",
+  },
+];
