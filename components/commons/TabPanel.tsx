7ab4c360f7bc3cfb3e4fd673cf5071cdc9f23223
Adi Wahyudi <adi.whyd02@gmail.com>
2024-07-07 13:51:16 +0700

    feat: crate TabPanel component

---
+++ components/commons/TabPanel.tsx
@@ -0,0 +1,23 @@
+import { Box, BoxProps } from "@mui/material";
+
+interface ITabPanelProps extends BoxProps {
+  value: string;
+  index: string;
+}
+
+export const TabPanel = ({
+  children,
+  value,
+  index,
+  ...props
+}: ITabPanelProps) => (
+  <Box
+    role="tabpanel"
+    hidden={value !== index}
+    id={`tabpanel-${index}`}
+    aria-labelledby={`tab-${index}`}
+    {...props}
+  >
+    {value === index && children}
+  </Box>
+);
